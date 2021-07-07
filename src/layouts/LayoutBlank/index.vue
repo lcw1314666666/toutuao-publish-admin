43cc8556913a373ff15c43bc3a2a0fef25ddf2ba
lcw1314666666 <1820566696@qq.com>
2021-07-07 20:07:55 +0200

    add layouts

---
+++ src/layouts/LayoutBlank/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h100">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutBlank'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/LayoutDefault/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-wrapper layout-default">
+    <default-header></default-header>
+    <slot />
+    <default-footer></default-footer>
+  </div>
+</template>
+
+<script>
+import DefaultHeader from '@/components/DefaultHeader'
+import DefaultFooter from '@/components/DefaultFooter'
+export default {
+  name: 'LayoutDefault',
+  components: {
+    DefaultHeader,
+    DefaultFooter
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
